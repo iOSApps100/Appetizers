3eefe81df36a064be99316056e6094a0e434ff40
iOSApps100 <contact.vikram92@gmail.com>
2024-09-04 16:00:26 +0530

    appetizerviewcell done

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,8 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Vivek  Garg on 04/09/24.
+//
+
+import Foundation
---
+++ Appetizers/AppetizerListCell.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerListCell.swift
+//  Appetizers
+//
+//  Created by Vivek  Garg on 04/09/24.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetizerListCell()
+}
