a5d92b2e475bbcfe10607dc31e3313c8755df68d
iOSApps100 <contact.vikram92@gmail.com>
2024-09-05 01:11:12 +0530

    created network manager and model

--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+//// final means other can not inherit it. Final will be for class types not value types.
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let appetizerURL = baseURL + "appetizers"
+    
+    private init() {}
+    
+    func getAppetizer(completion: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let error = error else {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+        }
+    }
+    
+    
+    
+    
+}
