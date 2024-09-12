6dde2cf92ed3680a6fc9ee9d345a4a1afcdd0dca
iOSApps100 <contact.vikram92@gmail.com>
2024-09-12 17:09:19 +0530

    Network call for downloading images

--- Appetizers/ImageLoader.swift
+++ Appetizers/ImageLoader.swift
@@ -5,4 +5,40 @@
 //  Created by Vivek  Garg on 12/09/24.
 //
 
-import Foundation
+import SwiftUI
+// Image is SwiftUI image and UIImage is UIkit image
+
+final class ImageLoader: ObservableObject {
+    // We made this property @published so when image changes happens it will go to its lisneres to redraw the UI
+    @Published var image: Image? = nil
+    
+    func load(fromURLString urlString: String) {
+        NetworkManager.shared.downloadImage(fromURLString: urlString) { uiImage in
+            guard let uiImage = uiImage else {return}
+            DispatchQueue.main.async {
+                self.image = Image(uiImage: uiImage) // Image(uiImage:
+            }
+        }
+    }
+}
+
+struct RemoteImage: View {
+    
+    var image: Image?
+    
+    var body: some View {
+        // cmd + shft + L for system images
+        image?.resizable() ?? Image(systemName: "cup.and.saucer").resizable()
+    }
+}
+
+struct AppetizerRemoteImage: View {
+    
+    @StateObject var imageLoader = ImageLoader()
+    let urlString: String
+    
+    var body: some View {
+        RemoteImage(image: imageLoader.image)//at first this is nil so onAppear called and triggered load function
+            .onAppear { imageLoader.load(fromURLString: urlString)}
+    }
+}
--- Appetizers/NetworkManager.swift
+++ Appetizers/NetworkManager.swift
@@ -2,15 +2,17 @@
 //  NetworkManager.swift
 //  Appetizers
 //
-//  Created by Vivek  Garg on 04/09/24.
+//  Created by Vikram Kumar on 04/09/24.
 //
 
-import Foundation
+import UIKit
 
 //// final means other can not inherit it. Final will be for class types not value types.
 final class NetworkManager {
     
     static let shared = NetworkManager()
+    /// To store images in cache, NSCache<key,value>.
+    private let cache = NSCache<NSString, UIImage>()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
@@ -49,6 +51,33 @@ final class NetworkManager {
         }.resume()
     }
     
+    func downloadImage(fromURLString urlString: String, completion: @escaping (UIImage?) -> Void) {
+        
+        /// impose check of cache image before downloading images from network image.
+        let cacheKey = NSString(string: urlString)
+        
+        //finding image for that key, key here we use is url of that image
+        if let image = cache.object(forKey: cacheKey) {
+            completion(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            
+            guard let data = data, let image = UIImage(data: data) else {
+                completion(nil)
+                return
+            }
+            /// we downloaded our image now set it cache first.
+            self.cache.setObject(image, forKey: cacheKey)
+            completion(image)
+        }.resume()
+    }
     
     
     
